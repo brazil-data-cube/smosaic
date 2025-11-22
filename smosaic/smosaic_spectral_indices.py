6d31867184d6d42d820e688238d24cef4748962e
sansigolo <gabrielsansigolo@gmail.com>
2025-11-21 23:27:50 -0300

    wip

--- smosaic/smosaic_spectral_indices.py
+++ smosaic/smosaic_spectral_indices.py
@@ -47,7 +47,7 @@ def ndbi_calc(band_1, band_2, compress='LZW'):
     
     band_1_resampled = band_1
     band_2_resampled = band_2
-    
+
     if dims_band_1 != dims_band_2:
         valid_widths = [d for d in [dims_band_1[0], dims_band_2[0]] if d is not None]
         valid_heights = [d for d in [dims_band_1[1], dims_band_2[1]] if d is not None]
@@ -78,8 +78,47 @@ def ndbi_calc(band_1, band_2, compress='LZW'):
         os.remove(band_2_resampled)
    
 def mndwi_calc(band_1, band_2, compress='LZW'):
-    pass
+    
+    output_file = band_2.replace("-B03-", "-MNDWI-")
+    
+    dims_band_1 = get_raster_dimensions(band_1)
+    dims_band_2 = get_raster_dimensions(band_2)
+    
+    if dims_band_1 is None or dims_band_2 is None:
+        return
+    
+    band_1_resampled = band_1
+    band_2_resampled = band_2
 
+    if dims_band_1 != dims_band_2:
+        valid_widths = [d for d in [dims_band_1[0], dims_band_2[0]] if d is not None]
+        valid_heights = [d for d in [dims_band_1[1], dims_band_2[1]] if d is not None]
+        
+        if not valid_widths or not valid_heights:
+            return
+            
+        target_width = max(valid_widths)
+        target_height = max(valid_heights)
+        
+        if dims_band_1[0] != target_width or dims_band_1[1] != target_height:
+            temp_band_1_resampled = f"temp_resampled_band_1_{os.path.basename(band_1)}"
+            resample_cmd_band_1 = f'gdalwarp -ts {target_width} {target_height} -r bilinear {band_1} {temp_band_1_resampled}'
+            os.system(resample_cmd_band_1)
+            band_1_resampled = temp_band_1_resampled
+        
+        if dims_band_2[0] != target_width or dims_band_2[1] != target_height:
+            temp_band_2_resampled = f"temp_resampled_band_2_{os.path.basename(band_2)}"
+            resample_cmd_band_2 = f'gdalwarp -ts {target_width} {target_height} -r bilinear {band_2} {temp_band_2_resampled}'
+            os.system(resample_cmd_band_2)
+            band_2_resampled = temp_band_2_resampled
+    
+    os.system(f'gdal_calc.py -A {band_1_resampled} -B {band_2_resampled} --outfile={output_file} --calc="where((A+B)==0,-9999,(B-A)/(B+A))" --type=Float32 --NoDataValue=-9999 --co COMPRESS={compress} --co BIGTIFF=IF_SAFER')
+    
+    if band_1_resampled != band_1 and os.path.exists(band_1_resampled):
+        os.remove(band_1_resampled)
+    if band_2_resampled != band_2 and os.path.exists(band_2_resampled):
+        os.remove(band_2_resampled)
+   
 def calculate_spectral_indices(input_folder: str, spectral_indices) -> str:
     for spectral_indice in spectral_indices:
 
@@ -167,7 +206,21 @@ def calculate_spectral_indices(input_folder: str, spectral_indices) -> str:
             ]
             
             for i in range(min(len(files_swir), len(files_nir))):
-                ndbi_calc(band_1=files_swir[i],band_2=files_nir[i],indice="NDBI")
+                ndbi_calc(band_1=files_swir[i],band_2=files_nir[i])
                         
         if spectral_indice == "MNDWI":
-            pass
+            
+            pattern_swir = r'-B11-'
+            files_swir = [
+                os.path.join(input_folder, f) for f in os.listdir(input_folder)
+                if re.search(pattern_swir, f)
+            ]
+            
+            pattern_green = r'-B03-'
+            files_green = [
+                os.path.join(input_folder, f) for f in os.listdir(input_folder)
+                if re.search(pattern_green, f)
+            ]
+
+            for i in range(min(len(files_swir), len(files_green))):
+                mndwi_calc(band_1=files_swir[i],band_2=files_green[i])
