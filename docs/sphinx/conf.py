7c17db33e635cbd659bda3c5d0fc7237dc5c7253
sansigolo <gabrielsansigolo@gmail.com>
2026-02-03 17:08:47 -0300

    add readthedocs

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -31,9 +31,7 @@ from unittest.mock import MagicMock
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath(".."))
-
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 # -- Project information -----------------------------------------------------
